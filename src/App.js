d1868db96c797224db66d671dd5d81e5a9da19f2
Kurt Hakim <kurthakim@gmail.com>
2021-06-25 01:07:42 +0700

    Add Dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 
 // const items = [
@@ -18,12 +19,42 @@ import Search from './components/Search';
 //     }
 // ];
 
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'A Shade of Blue',
+        value: 'blue'
+    }
+
+];
+
+
 // eslint-disable-next-line
 export default () => {
+
+    const [selected, setSelected] = useState(options[0]);
+    const [showDropdown, setShowDropdown] = useState(true);
+
+
     return (
         <div>
             {/* <Accordion items= { items }/> */}
-            <Search />
+            {/* <Search /> */}
+            <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
+            {showDropdown ?
+                <Dropdown 
+                selected={selected}
+                onSelectedChange={setSelected}
+                options={options} 
+            /> : null
+            }
         </div>
     );
 };
